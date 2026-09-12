b40d6d22677cab412b3ed18fe3a2f63f72b6cbe1
hetrank <hetrank@gmail.com>
2026-09-12 13:41:54 +0530

    Model Training and Evaluation done

--- GridSight-ML/src/gridsight/components/model_trainer.py
+++ GridSight-ML/src/gridsight/components/model_trainer.py
@@ -1,4 +1,7 @@
 import joblib
+import pandas as pd
+
+from pathlib import Path
 from xgboost import XGBRegressor
 
 
@@ -7,7 +10,23 @@ class ModelTrainer:
     def __init__(self, config):
         self.config = config
 
-    def train(self, train_df):
+    def initiate_model_training(self, train_path):
+
+        print("\nStarting model training...")
+
+        # ------------------------------------------------
+        # Load training data
+        # ------------------------------------------------
+
+        train_df = pd.read_csv(train_path)
+
+        print(
+            f"Training dataset shape: {train_df.shape}"
+        )
+
+        # ------------------------------------------------
+        # Separate features and target
+        # ------------------------------------------------
 
         X_train = train_df.drop(
             columns=["Generation_kW"]
@@ -17,28 +36,59 @@ class ModelTrainer:
             "Generation_kW"
         ]
 
+        print(
+            f"Number of features: {X_train.shape[1]}"
+        )
+
+        print(
+            f"Training samples: {X_train.shape[0]}"
+        )
+
+        # ------------------------------------------------
+        # Create XGBoost model
+        # ------------------------------------------------
+
+        print("\nInitializing XGBoost...")
+
         model = XGBRegressor(
             **self.config.model_params
         )
+
+        # ------------------------------------------------
+        # Train
+        # ------------------------------------------------
+
+        print("Training XGBoost model...")
 
         model.fit(
             X_train,
             y_train
         )
 
-        self.config.trained_model_path.parent.mkdir(
+        print(
+            "XGBoost training completed."
+        )
+
+        # ------------------------------------------------
+        # Save model
+        # ------------------------------------------------
+
+        model_path = Path(
+            self.config.trained_model_path
+        )
+
+        model_path.parent.mkdir(
             parents=True,
             exist_ok=True
         )
 
         joblib.dump(
             model,
-            self.config.trained_model_path
+            model_path
         )
 
         print(
-            f"Model saved to: "
-            f"{self.config.trained_model_path}"
+            f"Model saved to: {model_path}"
         )
 
         return model
